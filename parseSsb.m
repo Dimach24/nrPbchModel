e54e4fe3108c03ec66522108bfb2bcc5c0429685
Dim_Cham <dchaminov@ya.ru>
2025-03-24 15:10:41 +0300

    refactoring-[5g]: old code deleted

--- parseSsb.m
+++ parseSsb.m
@@ -39,22 +39,6 @@ function [pbch,dmrs]=parseSsb(rg,symbols_offset,subcarriers_offset,NCellId)
         dmrs(j)=rg(subcarriers_offset+1+kx,symbols_offset+4);
         j=j+1;
     end
-    % nu=mod(NCellId,4);
-    %
-    % % indexes of PBCH DM-Rs
-    % dmrs13index=(0:4:236)+nu;
-    % dmrs2index=[(0:4:44)+nu, (192:4:236)+nu];
-    % % indexes of PBCH DM-RS
-    % pbch13index = setdiff(0:239,dmrs13index);
-    % pbch2index  = setdiff([0:47 192:239],dmrs2index);
-    %
-    % rg=rg(subcarriers_offset+1:subcarriers_offset+240 ...
-    %     ,symbols_offset+2:symbols_offset+4);
-    % % 144 elements of dm-rs
-    % dmrs=[rg(dmrs13index+1,1); rg(dmrs2index+1,2); rg(dmrs13index+1,3)];
-    % % 540 elements of
-    % pbch?????????????????????????????????????????????????????????????????????????????
-    % pbch=[rg(pbch13index+1,1); rg(pbch2index+1,2); rg(pbch13index+1,3)];
     dmrs=dmrs.';
     pbch=pbch.';
 end
